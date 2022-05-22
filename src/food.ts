d78ac51136e604586bc3dc0b7a77b97ec4f8cc89
Darren Meiring <dpmeiring@gmail.com>
2022-05-22 15:33:50 +0800

    Updated food schema

--- src/food.ts
+++ src/food.ts
@@ -7,7 +7,14 @@ export const foodSchema = new Schema(Food, {
     food_profile_id: { type: 'text' },
     derivation: { type: 'text' },
     food_name: { type: 'text' },
-    food_description: { type: 'text' }
-
+    food_description: { type: 'text' },
+    sampling_details: { type: 'text' },
+    nitrogen_factor: { type: 'text' },
+    fat_factor: { type: 'text' },
+    specific_gravity: { type: 'text' },
+    analysed_portion: { type: 'text' },
+    unanalysed_portion: { type: 'text' },
+    classification: { type: 'text' },
+    classification_name: { type: 'text' }
 })
 
