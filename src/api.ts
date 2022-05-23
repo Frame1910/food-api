13be3ff84853dcbccf7320810662e2865a91ccc1
Darren Meiring <dpmeiring@gmail.com>
2022-05-23 12:24:17 +0800

    Updated to node-redis

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import connect from '.';
-import { foodSchema } from './food';
 
 
 export const app = express();
@@ -16,8 +15,15 @@ app.get('/test?', (req: Request, res: Response) => {
 
 app.get('/search?', async (req: Request, res: Response) => {
     const client = await connect();
-    const foodRepo = client.fetchRepository(foodSchema);
-    let foods = await foodRepo.searchRaw(req.query['term'].toString()).return.all();
+    let foods = await client.ft.search('Food:index', req.query['term'].toString(), {
+        RETURN: ['food_name', 'food_desc'],
+        HIGHLIGHT: {
+            TAGS: {
+                open: '<b>',
+                close: '</b>'
+            }
+        }
+    })
     res.send(foods);
-    await client.close();
+    await client.disconnect();
 })
