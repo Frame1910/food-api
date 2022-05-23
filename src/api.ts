35a31473a13b1523be80824eeab6a460deb3d884
Darren Meiring <dpmeiring@gmail.com>
2022-05-23 16:24:59 +0800

    Added scoring

--- src/api.ts
+++ src/api.ts
@@ -15,15 +15,31 @@ app.get('/test?', (req: Request, res: Response) => {
 
 app.get('/search?', async (req: Request, res: Response) => {
     const client = await connect();
-    let foods = await client.ft.search('Food:index', req.query['term'].toString(), {
-        RETURN: ['food_name', 'food_desc'],
+    let foods = await client.ft.search('Food:index', req.query['term'].toString() + '*', {
+        RETURN: ['class_name', 'food_name', 'food_desc', 'entry_type', 'public_food_key'],
         HIGHLIGHT: {
             TAGS: {
                 open: '<b>',
                 close: '</b>'
             }
+        },
+    })
+    let score: Array<any> = await client.sendCommand(['FT.SEARCH', 'Food:index', req.query['term'].toString() + '*', 'RETURN', '1', 'public_food_key', 'WITHSCORES', 'NOCONTENT'])
+    score.forEach((element, index, self) => {
+        if (index > 0) {
+           
+            if (index % 2 !== 0) {
+                let foodId = element;
+                let matchingIndex = foods.documents.findIndex(doc => doc.id == foodId);
+                if (matchingIndex > -1){
+                    foods.documents[matchingIndex]['score'] = parseInt(self[index+1]);
+                    console.log(foods.documents[matchingIndex]);
+                }
+            }
+
         }
     })
+    //foods.documents.sort((a, b) => b['score'] - a['score']);
     res.send(foods);
     await client.disconnect();
 })
