ac09ccf5ed9494e19abc09b8a85f9c96a4825f5e
Darren <dpmeiring@gmail.com>
2022-05-24 14:56:39 +0800

    Tuned search params

--- src/api.ts
+++ src/api.ts
@@ -15,8 +15,13 @@ app.get('/test?', (req: Request, res: Response) => {
 
 app.get('/search?', async (req: Request, res: Response) => {
     const client = await connect();
-    let foods = await client.ft.search('Food:index', req.query['term'].toString() + '*', {
+    let foods = await client.ft.search('Food:index', req.query['term'].toString(), {
         RETURN: ['class_name', 'food_name', 'food_desc', 'entry_type', 'public_food_key'],
+        SUMMARIZE: {
+            FIELDS: ['food_desc'],
+            FRAGS: 1,
+            LEN: 10,
+        }
         HIGHLIGHT: {
             TAGS: {
                 open: '<b>',
@@ -24,7 +29,7 @@ app.get('/search?', async (req: Request, res: Response) => {
             }
         },
     })
-    let score: Array<any> = await client.sendCommand(['FT.SEARCH', 'Food:index', req.query['term'].toString() + '*', 'RETURN', '1', 'public_food_key', 'WITHSCORES', 'NOCONTENT'])
+    let score: Array<any> = await client.sendCommand(['FT.SEARCH', 'Food:index', req.query['term'].toString(), 'RETURN', '1', 'public_food_key', 'WITHSCORES', 'NOCONTENT'])
     score.forEach((element, index, self) => {
         if (index > 0) {
            
@@ -33,13 +38,12 @@ app.get('/search?', async (req: Request, res: Response) => {
                 let matchingIndex = foods.documents.findIndex(doc => doc.id == foodId);
                 if (matchingIndex > -1){
                     foods.documents[matchingIndex]['score'] = parseInt(self[index+1]);
-                    console.log(foods.documents[matchingIndex]);
                 }
             }
 
         }
     })
-    //foods.documents.sort((a, b) => b['score'] - a['score']);
+    foods.documents.sort((a, b) => b['score'] - a['score']);
     res.send(foods);
     await client.disconnect();
 })
